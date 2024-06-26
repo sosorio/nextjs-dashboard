71fd33a54336b55e950b088d5e1cbb1bc2435a7b
Santiago Osorio <sosorio1973@gmail.com>
2024-06-26 09:48:23 -0500

    primer commit

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import { lusitana } from './ui/fonts';
+import './ui/global.css';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${lusitana.className} antialiased`}>
+        <h1 className='text-blue-500'>Este titulo</h1>
+        <div style={{border: '1px solid #000'}}>
+          {children}
+          </div>
+          <footer className='py-10 flex justify-center items-center'>Hecho con amor por la gente de Vercel. </footer>
+          </body>
+    </html>
+  );
+}
---
+++ app/ui/acme-logo.tsx
@@ -0,0 +1,13 @@
+import { GlobeAmericasIcon } from '@heroicons/react/24/outline';
+import { lusitana } from '@/app/ui/fonts';
+
+export default function AcmeLogo() {
+  return (
+    <div
+      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+    >
+      <GlobeAmericasIcon className="h-12 w-12 rotate-[15deg]" />
+      <p className="text-[28px]">FloresHoy</p>
+    </div>
+  );
+}
